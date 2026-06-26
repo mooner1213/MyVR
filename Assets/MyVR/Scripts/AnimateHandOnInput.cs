3621d0cf091e3644d00868ef589eda704bbcfc6d
mooner1213 <mooner948@gmail.com>
2026-06-26 18:54:11 +0900

    vr 양손 세팅, 기본 세팅 법

---
+++ Assets/MyVR/Scripts/AnimateHandOnInput.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace MyVR
+{
+    /// <summary>
+    /// 인풋 받아서 손 애니메이션 구현
+    /// </summary>
+    public class AnimateHandOnInput : MonoBehaviour
+    {
+        #region Variables
+        // 참조
+        private Animator animator;
+
+        public InputActionProperty pinchAnimationAction;
+        public InputActionProperty gripAnimationAction;
+
+        // 애니 파라미터
+        private string grip = "Grip";
+        private string trigger = "Trigger";
+        #endregion
+
+        #region Unity Event Method
+
+        private void Update()
+        {
+            float triggerValue = pinchAnimationAction.action.ReadValue<float>();
+            float gripValue = gripAnimationAction.action.ReadValue<float>();
+
+            animator.SetFloat(trigger, triggerValue);
+            animator.SetFloat(grip, gripValue);
+        }
+
+        #endregion
+    }
+}
